dcfaef081272949232b2c4740eb2ae7549879968
nurid <nurkh4mid@yahoo.com>
2023-03-01 23:25:18 +0700

    React JSX section, pp 23-27 done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
 
+const title = 'React Reactor';
+const welcome = { title: "React", greeting: "Hey..."};
+
+function getSound(sound) {
+  return sound;
+}
+
+const ar = ["makan", "nasi", "goreng"];
+
 function App() {
 
   return (
    <div>
-     <h1>Hello React</h1>
+     <h1>Hello {title}</h1>
+
+     <label htmlFor="search">Search: </label>
+     <input id="search" type="text" />
+
+     <h1>{welcome.title} {welcome.greeting}</h1>
+
+     <h1>Hello {getSound("Tiger")}</h1>
+
+     <h1> Mapping array ar</h1>
+     <ul>
+       {ar.map((val, index) => <li>{++index}. {val}</li>)}
+     </ul>
    </div>
   )
 }
