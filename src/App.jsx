33835a2fea2addbcbe06c856dd3904f2140e788a
nurid <nurkh4mid@yahoo.com>
2023-03-05 09:56:01 +0700

    Meet another React Component, to page 41.

--- src/App.jsx
+++ src/App.jsx
@@ -63,8 +63,44 @@ function App() {
         );
       })}
      </ul>
-   </div>
+
+     <hr />
+
+     <h1>List component</h1>
+     <List />
+
+      <h1>Search component</h1>
+      <Search />
+  </div>
+
   )
 }
 
+function Search() {
+  return (
+    <div>
+     <label htmlFor="search">Search: </label>
+     <input id="search" type="text" />
+    </div>
+  )
+}
+
+function List() {
+  return (
+     <ul>
+      {list.map(function(item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+     </ul>
+  )
+}
 export default App
